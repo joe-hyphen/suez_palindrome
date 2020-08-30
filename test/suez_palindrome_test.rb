a76b9b534a296673b9bef8d4e0e0230a6e513a0c
Chair Umpire <retroprophet@yahoo.com>
2020-08-30 12:07:30 -0400

    8.3 Red

--- test/suez_palindrome_test.rb
+++ test/suez_palindrome_test.rb
@@ -3,10 +3,22 @@ require "test_helper"
 class SuezPalindromeTest < Minitest::Test
 
   def test_non_palindrome
-    assert !"apple".palindrome?
+    refute "apple".palindrome?
   end
 
   def test_literal_palindrome
     assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
   end
 end
